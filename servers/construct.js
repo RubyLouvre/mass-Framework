95bda1e61bf5e09f551c030173c94ff59d7e4801
RubyLouvre <cheng19840218@gmail.com>
2011-12-17 02:06:53 +0800

    一些工具函数

---
+++ servers/construct.js
@@ -0,0 +1,21 @@
+mass.define("construct", function(){
+    mass.mix(mass,{
+        intercepter : function(fn){//拦截器的外壳
+            return function(req, res, err){
+                if(err ){
+                    req.emit("next_intercepter", req, res, err);
+                }else if(fn(req,res) === true){
+                    req.emit("next_intercepter", req, res)
+                }
+            }
+        },
+        //用于修正路径的方法,可以传N个参数
+        adjustPath : function(){
+            [].unshift.call(arguments,mass.settings.approot, mass.settings.appname);
+            return require("path").join.apply(null,arguments)
+        }
+    })
+
+    
+})
+
