407835500873856bcd54e6c7fa8ef9fa73706b25
RubyLouvre <cheng19840218@gmail.com>
2011-12-17 02:09:35 +0800

    添加一系列模板文件

--- servers/templates/config/routes.js
+++ servers/templates/config/routes.js
@@ -3,25 +3,23 @@
 mass.define("routes",function(){
     return function(map){
         //方法路由
-        //        map.get('/','site#index');
-        //        map.get('/get_comments/:post_id','site#get_comments');
-        //        map.post('/add_comment','site#add_comment');
-        //        //资源路由
-        //   map.resources('posts');
-        //        map.resources('users');
-        //        map.get('/view/:post_name','site#view_post');
-        //        map.get('/rss','site#rss');
+        mass.log("<code style='color:yellow'>-------------------</code>",true);
+//        map.get('/','site#index');
+//        map.get('/get_comments/:post_id','site#get_comments');
+//        map.post('/add_comment','site#add_comment');
+//        //资源路由
+//        map.resources('posts');
+//        map.resources('users');
+//        map.get('/view/:post_name','site#view_post');
+//        map.get('/rss','site#rss');
 
 
-        // map.resources('posts', {path: 'articles', as: 'stories'});
-        //嵌套路由
-        //        map.resources('posts', function (post) {
-        //            post.resources('users');
-        //        });
-        //命名空间路由
-        map.namespace("tests",function(tests){
-            tests.resources('comments');
-        })
+    // map.resources('posts', {path: 'articles', as: 'stories'});
+    //嵌套路由
+            map.resources('post', function (post) {
+                post.resources('comments');
+            });
+    //
     //        map.resources('users', {
     //            only: ['index', 'show']
     //        });
