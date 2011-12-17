3f789a22121668d954fe9e5a131a182017764883
RubyLouvre <cheng19840218@gmail.com>
2011-12-17 11:59:46 +0800

    kill

--- servers/heredocument.js
+++
@@ -1,9 +0,0 @@
-mass.define("heredocument",function(){
-    //http://stackoverflow.com/questions/805107/multiline-strings-in-javascript
-    //只允许最前面出现一个 "\*" ,只允许最后面出现一个 "*／" 
-    mass.hereDoc = function(fn) {
-        return fn.toString().
-        replace(/^[^\/]+\/\*!?/, '').
-        replace(/\*\/[^\/]+$/, '');
-    }
-})
