642dde1eda413101415b3e1f96ddca9e34f87baf
RubyLouvre <cheng19840218@gmail.com>
2012-01-31 08:07:38 +0800

    rename

---
+++ rename.js
@@ -0,0 +1,28 @@
+var fs = require("fs")
+function getAllFiles(root) {
+  var result = [], files = fs.readdirSync(root)
+  files.forEach(function(file) {
+    var pathname = root+ "/" + file
+      , stat = fs.lstatSync(pathname)
+    if (stat === undefined) return
+ 
+    // �����ļ��о����ļ�
+    if (!stat.isDirectory()) {
+      result.push(pathname)
+    // �ݹ�����
+    } else {
+      result = result.concat(getAllFiles(pathname))
+    }
+  });
+  return result
+}
+var arr = getAllFiles("doc")
+arr.forEach(function(name){
+  console.log(name)
+  fs.readFile(name,function(e,fd){
+     var text = ( fd+"").replace('<!doctype html>\n<!DOCTYPE html>',"<!DOCTYPE html>");
+     fs.writeFile(name,text, function(e){
+     })
+  })
+
+})
