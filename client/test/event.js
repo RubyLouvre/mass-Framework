95a04848f48719d2785c88a6a9566ca9f0fc810e
RubyLouvre <cheng19840218@gmail.com>
2012-02-06 22:33:17 +0800

    增加DOM事件的测试

---
+++ client/test/event.js
@@ -0,0 +1,37 @@
+$.define("event","more/spec,event",function(){
+    var iframe =  $("<iframe id='test_event' style='display:none;width:0px;height:0px;' src='/test/event.html' frameBorder=0  />" ).appendTo("body");//
+    window.eventTestCall = function(){
+        var idoc = iframe.contents()[0];
+        $.fixture("DOM事件模块-event",{
+            "native fire": function(){
+                var form = $("#form1",idoc);
+                form.submit(function(e){
+                    expect( e.type ).eq( "submit" )
+                });
+                var input = $("#input1",idoc);
+                input.change(function(e){
+                    expect( e.type ).eq( "change" )
+                });
+                var select = $("#select1",idoc);
+                select.change(function(e){
+                    expect( e.type ).eq( "change" )
+                });
+
+                function fireEvent(el, type, e){
+                    if ( document.dispatchEvent ){
+                        e = document.createEvent("HTMLEvents");
+                        e.initEvent(type, true, true );
+                        el.dispatchEvent( e );
+                    }else {
+                        el.fireEvent( 'on'+type );
+                    }
+                }
+
+                fireEvent(form[0], "submit")
+                fireEvent(input[0], "change")
+                fireEvent(select[0], "change")
+
+            }
+        })
+    }
+})
