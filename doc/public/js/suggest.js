0185dae5face0abba1b4df8a688849b094441baa
RubyLouvre <cheng19840218@gmail.com>
2012-02-16 01:30:45 +0800

    进一步去掉冗余变量

--- doc/public/js/suggest.js
+++ doc/public/js/suggest.js
@@ -1,14 +1,12 @@
 $.require("ready,event,fx",function(){
-    var search = $("#search"),hash = window, prefix = "";
-    search.input(function(e){//监听输入
+    var search = $("#search"), hash = window, prefix = "";
+    search.input(function(){//监听输入
         var
         input = this.value,//原始值
         val = input.slice( prefix.length),//比较值
-        output = [], //用来放置输出内容
-        apis = []; //放置方法或属性名
+        output = []; //用来放置输出内容
         for(var prop in hash){
-            if( prop.indexOf( val ) === 0  ){
-                apis.push( prop );
+            if( prop.indexOf( val ) === 0  ){//取得以输入值开头的API
                 if( output.push( '<li><a href="javascript:void(0)" data-value="'+prefix +
                     prop+'">'+ input + "<b>" + (prefix  + prop ).slice( input.length ) +"</b></a></li>" ) == 10){
                     break;
@@ -26,7 +24,6 @@ $.require("ready,event,fx",function(){
             }
             prefix = input == "." ? "" : input;
             for( prop in hash){
-                apis.push( prop );
                 if( output.push( '<li><a href="javascript:void(0)" data-value="'+prefix +
                     prop+'">'+ input + "<b>" + (prefix + prop ).slice( prefix.length ) +"</b></a></li>" ) == 10){
                     break;
@@ -36,38 +33,38 @@ $.require("ready,event,fx",function(){
         $("#suggest_list").html( output.join("") );
         if(!input){
             hash = window;
-            prefix = output = apis = [];
+            prefix = output = [];
         }
     });
     var glowIndex = -1;
     $(window).keyup(function(e){//监听上下翻
-        e.preventDefault();
         var upOrdown = 0
         if(e.which === 38 || e.which === 104){ //up 8
-            upOrdown = -1;
-        }else if(e.which === 40 || e.which === 98){//down
-            upOrdown = +1;
+            upOrdown --;
+        }else if(e.which === 40 || e.which === 98){//down 2
+            upOrdown ++;
         }
         if(upOrdown){
             var list =  $("#suggest_list a");
+            //转移高亮的栏目
             list.eq(glowIndex).removeClass("glow_suggest");
             glowIndex += upOrdown;
             var el = list.eq( glowIndex ).addClass("glow_suggest");
             search.val( el.attr("data-value") )
-            if(glowIndex == list.length - 1){
+            if(glowIndex === list.length - 1){
                 glowIndex = -1;
             }
         }
     });
     var dd = $("#leftsection dd");
     search.keyup(function(e){//监听提交
-        var input = this.value
+        var input = this.value;
         if(input && (e.which == 13 || e.which == 108)){ //如果按下ENTER键
             for(var i = 0 , el ; el = dd[i++];){
                 var path = $(el).attr("path");
                 var start = path.indexOf("/");
                 if( path.slice(start+1).indexOf(input) === 0){
-                    $("#iframe").attr("src", "/doc/"+path );
+                    $("#iframe").attr("src", "/doc/"+path );//更新iframe
                     break;
                 }
             }
