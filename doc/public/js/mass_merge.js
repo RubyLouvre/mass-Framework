92ecf3b5d6f34fabc31a9f9125f0ad738e8d7028
RubyLouvre <cheng19840218@gmail.com>
2012-02-19 22:35:56 +0800

    更新合并后的文件

--- doc/public/js/mass_merge.js
+++ doc/public/js/mass_merge.js
@@ -73,7 +73,7 @@
         rword: /[^, ]+/g,
         mass: mass,//大家都爱用类库的名字储存版本号，我也跟风了
         "@name": "$",
-        "@debug": false,
+        "@debug": true,
         "@target": w3c ? "addEventListener" : "attachEvent",
         "@path": (function( url, scripts, node ){
             scripts = DOC.getElementsByTagName( "script" );
@@ -398,7 +398,7 @@
 var module_value = {
                                     state:2
                                 };
-                                var list = "lang_fix,lang,support,class,data,query,node,css_fix,css,attr,target,event,fx,more/placeholder".match($.rword);
+                                var list = "lang_fix,lang,support,class,data,query,node,css_fix,css,attr,target,event,fx,more/placeholder,more/flip".match($.rword);
                                 for(var i=0, module;module = list[i++];){
                                     mapper["@"+module] = module_value;
                                 }//=========================================
@@ -5282,7 +5282,6 @@ $.define("fx", "css",function(){
             props.after = duration;
             duration = null;
         }
-        console.log(props)
         for( var name in props){
             p = $.cssName(name) || name;
             if( name != p ){
@@ -5297,8 +5296,6 @@ $.define("fx", "css",function(){
         config.easing = $.easing[ easing ] ? easing : "swing";
         config.duration = duration || 500;
         config.method = "noop";
-        console.log("$.fn.fx");
-        console.log(props)
         return this.each(function(node){
             var linked = $._data(node,"fx") || $._data( node,"fx",{
                 positive: [], //正向列队
@@ -5378,9 +5375,9 @@ $.define("fx", "css",function(){
                 }
 
             } else { // 初始化动画
+                fx.render = fxBuilder(node, linked, props, config); // 生成补间动画函数
                 mix = config.before
                 mix && (mix.call( node, node, fx.props, fx ), config.before = 0);
-                fx.render = fxBuilder(node, linked, props, config); // 生成补间动画函数
                 $[ config.method ].call(node, node, props, fx );//供show, hide 方法调用
                 fx.startTime = now = +new Date;
             }
@@ -5447,7 +5444,7 @@ $.define("fx", "css",function(){
                     return end - from[i]
                 });
             }
-            if(from +"" === to +"" || /NaN/.test(from) || /NaN/.test(to)){//不处理初止值都一样的样式与属性
+            if(from +"" === to +"" ){//不处理初止值都一样的样式与属性
                 continue;
             }
             var hash = {
@@ -5625,7 +5622,6 @@ $.define("fx", "css",function(){
                     }
                 }
             }
-            console.log(props)
             return nodes.fx(duration, props);
         },
         show: function(node, props){
@@ -5828,13 +5824,13 @@ $.define("placeholder","attr,css,event",function(){
     //读者可以与这个比较一下https://github.com/danielstocks/jQuery-Placeholder/blob/master/jquery.placeholder.js
     //http://www.iliadraznin.com/2011/02/jquery-placeholder-plugin/
   //  $.log("placeholder模块加载成功")
-    function fix(input, node, val) {
+    function fix(input, node, val) {//v3
         var placeholder = $._data(node,"placeholder")
         if(!placeholder){
-            placeholder = $("<kbd>").afterTo(input).css({
-                position: "relative",
-                left: -1 * (input.innerWidth() + parseFloat(input.css("marginRight"))) ,
-                top:  -1 * parseFloat(input.css("paddingTop")),
+            placeholder = $("<kbd>").css({
+                position: "absolute",
+                left:  input.offset().left + 2,
+                top:   input.offset().left + 2,
                 display: "inline-block",
                 w: input.width() - 4 ,
                 h: input.height(),
@@ -5844,7 +5840,7 @@ $.define("placeholder","attr,css,event",function(){
                 c: "#808080",
                 "font-weight": input.css("font-weight"),
                 "font-size": input.css("font-size")      
-            });
+            }).appendTo("body")
             $._data( node,"placeholder", placeholder )
         }
         return placeholder.text(val);
@@ -5893,6 +5889,177 @@ $.define("placeholder","attr,css,event",function(){
     }
 });
 
+//2012.2.17 v1 插入到目标元素之后,然后负边距定位
+//2012.2.18 v2 插入到目标元素之后,然后相对定位, 增加对坐标,边框, 字体的样式处理
+//2012.2.19 v3 插入到body之内,然后绝对定位
+$.define("flip", "fx", function(){
+    var flip = {
+        begin: function() {
+            hyaline =  (!"1"[0] ? "#123456" : "transparent")
+            return {//初始化属性
+                hyaline: hyaline,
+                backgroundColor: hyaline,
+                fontSize:0,
+                lineHeight:0,
+                borderTopWidth:0,
+                borderLeftWidth:0,
+                borderRightWidth:0,
+                borderBottomWidth:0,
+                borderTopColor: hyaline,
+                borderBottomColor: hyaline,
+                borderLeftColor: hyaline,
+                borderRightColor: hyaline,
+                background: "none",
+                borderStyle: 'solid',
+                height:0,
+                width:0
+            };
+        },
+        horizontal: function( obj ){
+            var waist = obj.height /100 *25;
+            var begin = flip.begin();
+            begin.width= obj.width;//将初始属性克隆成三份，并逐渐改变它们
+            return {
+                hyaline: begin.hyaline,
+                begin: begin,
+                middle: {
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0,
+                    borderLeftWidth: waist,
+                    borderRightWidth: waist,
+                    borderTopColor: "#999",
+                    borderBottomColor:"#999",
+                    top:  obj.top + (obj.height/2),//向上移
+                    left: obj.left - waist//向左移
+                },
+                end: {
+                    borderBottomWidth: 0,
+                    borderTopWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderTopColor: begin.hyaline,
+                    borderBottomColor: begin.hyaline,
+                    top: obj.top,
+                    left: obj.left
+                }
+            };
+        },
+        vertical:  function ( obj ) {
+            var waist = obj.height/100 * 25;
+            var begin =  flip.begin();
+            begin.height = obj.height;
+            return {
+                hyaline: begin.hyaline,
+                begin : begin,
+                middle : {
+                    borderTopWidth: waist,
+                    borderBottomWidth: waist,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderLeftColor: "#999",
+                    borderRightColor: "#999",
+                    top: obj.top-waist,
+                    left: obj.left+ (obj.width/2)
+                },
+                end: {
+                    borderTopWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderBottomWidth: 0,
+                    borderLeftColor: begin.hyaline,
+                    borderRightColor: begin.hyaline,
+                    top: obj.top,
+                    left: obj.left
+                }
+            };
+        }
+    }
+    var dirMap = {
+        t: "Top",
+        b: "Bottom",
+        l: "Left",
+        r: "Right"
+    }
+    function getDirOption( obj, dir, orientation ){
+        var result = flip[ orientation ]( obj ), arr = ["begin", "end"]
+        dir.replace( $.rword, function( who ){
+            var which = arr.shift();
+            result[ which ]["border"+ who + "Width"] = obj[ orientation === "horizontal" ? "height" : "width"];
+            result[ which ]["border"+ who + "Color"] = obj[which + "Bgc"];
+        });
+        return result;
+    }
+    //hash 中的主要参数 beginBgc endBgc before after frame, direction
+    $.fn.flip = function(duration, hash){//并不是原对象进行动画,而是其克隆进行动画
+        var props =  hash || duration || {}
+        if(typeof duration === "function"){// fx(obj fn)
+            hash = duration;               // fx(obj, 500, fn)
+            duration = 500;
+        }
+        if(typeof hash === "function"){   //  fx(obj, num, fn)
+            props.after = hash;           //  fx(obj, num, {after: fn})
+        }
+        var direction = props.direction || "tb";
+        direction = dirMap[ direction.charAt(0) ] + ","+dirMap[ direction.charAt(1) ]
+        duration = Math.floor( duration / 2 );
+        var orientation = direction.charAt(1) === "o" ?  "horizontal" : "vertical" ;
+        return this.each(function(){
+            var $this = $(this);
+            if($this.data('fliping')){
+                return false;
+            }
+            $this.data("fliping", 1);
+            //取得原有对象中的有用的信息
+            var message = {
+                width: $this.width(),
+                height: $this.height(),
+                beginBgc: hash.beginBgc || $this.css("bgc"),
+                endBgc: hash.endBgc || "#999",
+                top: $this.offset().top,
+                left: $this.offset().left
+            };
+            //添加替身
+            var $clone = $this.css("visibility","hidden")
+            .clone(true)
+            .data('fliping',1)
+            .appendTo("body")
+            .html("")
+            .css({
+                visibility: "visible",
+                position: "absolute",
+                left: message.left,
+                top: message.top,
+                margin: 0,
+                zIndex: 9999,
+                boxShadow:"0px 0px 0px #000"
+            });
+
+            // console.log({visibility:"visible",position:"absolute",left:flipObj.left,top:flipObj.top,margin:0,zIndex:9999,boxShadow:"0px 0px 0px #000"})
+            var dirOption = getDirOption( message, direction, orientation);
+           if(!"1"[0]){//tb h
+                 dirOption.begin.filter = "chroma(color=" + dirOption.hyaline + ")";
+         }
+            var middle = dirOption.middle
+            middle.before = props.before || $.noop;
+            middle.frame = props.frame || $.noop;
+            var end = dirOption.end, self = this;
+
+            end.frame = function( clone, prop, fx ){
+                if(typeof props.frame === "function" ){
+                    props.frame.call( self, clone, prop, fx );
+                }
+            }
+            end.after = function( clone, prop, fx){
+                $this.css("visibility", "visible").removeData('fliping');
+                $clone.remove();
+                if(typeof props.after === "function" ){
+                    props.after.call( self, clone, prop, fx );
+                }
+            }
+            $clone.css( dirOption.begin ).fx( duration, middle).fx( duration, dirOption.end );
+        })
+    }
+})
 
 
 })( this, this.document );
