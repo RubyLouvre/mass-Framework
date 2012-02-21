6481ae1e3b62e3cd751637eafadbe76ae8bf900c
RubyLouvre <cheng19840218@gmail.com>
2012-02-22 02:08:36 +0800

    修正分页的内容!~

--- client/more/pagination.js
+++ client/more/pagination.js
@@ -4,6 +4,7 @@ $.define("pagination","event",function(){
         show_first:true,
         show_prev: true,
         show_next: true,
+        link_class: "link",
         prev_class: "prev_page",
         next_class: "next_page",
         next_text: "下一页&gt;",
@@ -40,7 +41,7 @@ $.define("pagination","event",function(){
         render: function(){
             var max = this.total_pages = Math.ceil( this.total / this.per_page),//计算总页数
             count = this.show_pages = Math.min( this.show_pages, this.total_pages ),//计算还剩下多少页要生成
-            curr = this.curr_page, add_more = false;
+            curr = this.curr_page, add_more = false, link = this.link_class;
             curr = this.curr_page = curr < 1 ? 1 : curr > this.total_pages ? this.total_pages : curr;
             //生成当前页
             var html = [ createLink( "span", curr ,curr,"current_page") ], left = curr-1,  right = curr + 1;
@@ -48,35 +49,35 @@ $.define("pagination","event",function(){
             //当成中间要显示的页
             while( count > 0 ) {
                 if( left >= 1 && count != 0 ) {//在日常生活是以1开始的
-                    html.unshift( createLink( "a", left--, left+1, "link") );
+                    html.unshift( createLink( "a", left--, left + 1, link ) );
                     --count
                 }
                 if( right <= max && count != 0 ) {
-                    html.push( createLink( "a", right, right++ , "link") );
+                    html.push( createLink( "a", right, right++ , link ) );
                     --count;
                 }
             }
             var space = left ;
             if( space > 1 ){//如果有至少两个位置,则可以用它放置第一页与省略号
-                html.unshift( createLink( "code",0,this.fill_text) );//添加省略号
+                html.unshift( createLink( "code", 0, this.fill_text ) );//添加省略号
                 add_more = true;//如果有省略号肯定能向前翻
             }
             if( space >= 1 && this.show_first ) {//只要留有至少一个空位,就可以显示最后一页
-                html.unshift( createLink("a", 1, 1, "link" ) );
+                html.unshift( createLink("a", 1, 1, link ) );
             }
             if( add_more  && this.show_prev ) {//如果允许显示上一页
-                html.unshift( createLink("a", curr - 1,this.prev_text,"prev_page") );
+                html.unshift( createLink("a", curr - 1, this.prev_text, this.prev_class ) );
             }
             space = max - (right-1), add_more = false;
             if( space > 1 ) {//如果有至少两个位置,则可以用它放置最后一页与省略号
-                html.push( createLink( "code", 0 ,this.fill_text ) );//添加省略号
+                html.push( createLink( "code", 0, this.fill_text ) );//添加省略号
                 add_more = true;//如果有省略号肯定能向后翻
             }
             if( space >= 1 && this.show_last ) {//只要留有至少一个空位,就可以显示最后一页
-                html.push( createLink( "a",max, max,"link" ) );
+                html.push( createLink( "a",max, max, link ) );
             }
             if( add_more  && this.show_next ) {//如果允许显示下一页
-                html.push( createLink( "a", curr + 1,this.next_text,"next_page") );
+                html.push( createLink( "a", curr + 1, this.next_text, this.next_class ) );
             }
             if( this.show_jump ){
                 html.push( "<kbd>跳转到第<input \/>页<\/kbd>" );
