4bddd601fd3d5c7a94d1f2b151b62e78ada52139
RubyLouvre <cheng19840218@gmail.com>
2012-02-26 11:09:14 +0800

    类工厂模块升级到  v8

--- client/class.js
+++ client/class.js
@@ -4,127 +4,132 @@
 $.define("class", "lang",function(){
     // $.log("已加载class模块")
     var
-    P = "prototype",  C = "constructor", I = "@init",S = "_super",
-    unextend = $.oneObject([S,P, 'extend', 'implement','_class']),
-    exclusive = new RegExp([S,I,C].join("|")),ron = /on([A-Z][A-Za-z]+)/,
+    unextend = $.oneObject(["_super","prototype", 'extend', 'implement' ]),
+    rconst = /constructor|_init|_super/,
     classOne = $.oneObject('Object,Array,Function');
     function expand(klass,props){
-        'extend,implement'.replace($.rword, function(name){
+        'extend,implement'.replace( $.rword, function(name){
             var modules = props[name];
-            if(classOne[$.type(modules)]){
-                klass[name].apply(klass,[].concat(modules));
+            if( classOne[ $.type( modules) ] ){
+                klass[name].apply( klass,[].concat( modules ) );
                 delete props[name];
             }
         });
-        return klass
+        return klass;
     }
-    function setOptions(){
-        var first = arguments[0]
-        if(typeof first === "string"){
-            first = this[first] || (this[first] = {});
-            [].splice.call(arguments, 0, 1, first);
-        }else{
-            [].unshift.call(arguments,this);
-        }
-        var target = $.Object.merge.apply(null,arguments), key, match
-        if (typeof this.bind == "function") {
-            for (key in target) {
-                if ((match = key.match(ron))) {
-                    this.bind(match[1].toLowerCase(), target[key]);
-                    delete(target[key]);
-                }
-            }
-        }
-        return this;
-    }
-    function _super(){
-        var caller = arguments.callee.caller;  // 取得当前方法
-        var name = caller._name;  // 取得当前方法名
-        var superclass = caller._class[S];//取得当前实例的父类
-        if(superclass && superclass[P][name] ){
-            return superclass[P][name].apply(this, arguments.length ? arguments : caller.arguments);
-        }else{
-            throw name + " no super method!"
-        }
-    }
-    $["@class"] =  {
-        inherit : function(parent,init) {
+
+    $["@class"] = {
+        inherit : function( parent,init ) {
             var bridge = function() { }
-            if(typeof parent == "function"){
+            if( typeof parent == "function"){
                 for(var i in parent){//继承类成员
-                    this[i] = parent[i]
+                    this[i] = parent[i];
                 }
-                bridge[P] = parent[P];
-                this[P] = new bridge ;//继承原型成员
-                this[S]  = parent;//指定父类
+                bridge.prototype = parent.prototype;
+                this.prototype = new bridge ;//继承原型成员
+                this._super = parent;//指定父类
             }
-            this[I] = (this[I] || []).concat();
-            if(init){
-                this[I].push(init);
+            this._init = (this._init || []).concat();
+            if( init ){
+                this._init.push(init);
             }
             this.toString = function(){
                 return (init || bridge) + ""
             }
-            var KP = this[P];
-            KP.setOptions = setOptions;
-            KP[S] = _super;//绑定方法链
-            return  KP[C] = this;
+            var proto = this.prototype;
+            proto.setOptions = function(){
+                var first = arguments[0];
+                if( typeof first === "string" ){
+                    first =  this[first] || (this[first] = {});
+                    [].splice.call( arguments, 0, 1, first );
+                }else{
+                    [].unshift.call( arguments,this );
+                }
+                $.Object.merge.apply(null,arguments);
+                return this;
+            }
+            return proto.constructor = this;
         },
         implement:function(){
-            var target = this[P], reg = exclusive;
+            var target = this.prototype, parent = this._super, reg = rconst;
             for(var i = 0, module; module = arguments[i++]; ){
                 module = typeof module === "function" ? new module :module;
                 Object.keys(module).forEach(function(name){
-                    if(!reg.test(name)){
+                    if( !reg.test(name) ){
                         var prop = target[name] = module[name];
                         if(typeof prop == "function"){
-                            prop._name  = name;
-                            prop._class = this;
+                            target[name] = (function(){
+                                var _super = function() {
+                                    return parent.prototype[ name ].apply( this, arguments );
+                                };
+                                var _superApply = function( args ) {
+                                    return parent.prototype[ name ].apply( this, args );
+                                };
+                                return function() {
+                                    var __super = this._super,
+                                    __superApply = this._superApply,
+                                    returnValue;
+
+                                    this._super = _super;
+                                    this._superApply = _superApply;
+
+                                    returnValue = prop.apply( this, arguments );
+
+                                    this._super = __super;
+                                    this._superApply = __superApply;
+
+                                    return returnValue;
+                                };
+                            })();
+                            target[name].toString = function(){
+                                return prop +"";
+                            }
                         }
                     }
-                },this);
+                }, this );
             }
             return this;
         },
         extend: function(){//扩展类成员
             var bridge = {}
             for(var i = 0, module; module = arguments[i++]; ){
-                $.mix(bridge, module);
+                $.mix( bridge, module );
             }
-            for(var key in bridge){
-                if(!unextend[key]){
+            for( var key in bridge ){
+                if( !unextend[key] ){
                     this[key] =  bridge[key]
                 }
             }
             return this;
         }
     };
-    $.factory = function(obj){
+    $.factory = function( obj ){
         obj = obj || {};
-        var parent  = obj.inherit //父类
-        var init = obj.init ; //构造器
+        var parent = obj.inherit //父类
+        var init = obj.init ;    //构造器
         delete obj.inherit;
         delete obj.init;
         var klass = function () {
-            for(var i = 0 , init ; init =  klass[I][i++];){
+            for( var i = 0 , init ; init =  klass._init[i++]; ){
                 init.apply(this, arguments);
             }
         };
-        $.mix(klass,$["@class"]).inherit(parent, init);//添加更多类方法
-        return expand(klass,obj).implement(obj);
+        $.mix( klass, $["@class"] ).inherit( parent, init );//添加更多类方法
+        return expand( klass, obj ).implement( obj );
     }
 });
 
-//2011.7.11
-//dom["class"]改为dom["@class"]
+//2011.7.11 将$["class"]改为$["@class"] v4
 //2011.7.25
 //继承链与方法链被重新实现。
-//在方法中调用父类的同名实例方法，由$super改为supermethod，保留父类的原型属性parent改为superclass
+//在方法中调用父类的同名实例方法，由$super改为supermethod，保留父类的原型属性parent改为superclass v5
 //2011.8.6
-//在方法中调用父类的同名实例方法，由supermethod改为_super，保留父类的原型属性superclass改为_super
+//在方法中调用父类的同名实例方法，由supermethod改为_super，保留父类的原型属性superclass改为_super v6
 //重新实现方法链
 //fix 子类实例不是父类的实例的bug
 //2011.8.14 更改隐藏namespace,增强setOptions
-//2011.10.7 include更名为implement 修复implement的BUG（能让人重写toString valueOf方法）
-//2012.1.29  修正setOptions中$.Object.merge方法的调用方式
+//2011.10.7 include更名为implement 修复implement的BUG（能让人重写toString valueOf方法） v7
+//2012.1.29 修正setOptions中$.Object.merge方法的调用方式
+//2012.2.25 改进setOptions，可以指定在this上扩展还是在this.XXX上扩展
+//2012.2.26 重新实现方法链，抛弃arguments.callee.caller   v8
 
