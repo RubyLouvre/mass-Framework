0c8567adacfbee3c542a87a592fa547e78a855b4
RubyLouvre <cheng19840218@gmail.com>
2012-03-08 18:21:54 +0800

    升级switchable

--- src/more/switchable.js
+++ src/more/switchable.js
@@ -7,11 +7,13 @@ $.define("switchable","more/uibase,event,attr,fx",function(Widget){
         active_callback: $.noop,
         pause_over_panel: false,
         autoplay: false,
+        switch_in: "slideDown",
+        switch_out: "slideUp",
         delay: 500
     }
 
     var createHTML = function( data, ui){
-        var t = $.tag, html = '<div class="mass_accordion">'
+        var t = $.tag, html = '<div class="mass_switchable">'
         for(var i =0 ,el; el = data[i++];){
             html += t("div class="+ui.trigger_class, el.trigger) +
             t("div class="+ui.panel_class, el.panel)
@@ -23,9 +25,14 @@ $.define("switchable","more/uibase,event,attr,fx",function(Widget){
         ui.triggers = ui.target.find("." + ui.trigger_class );
         ui.panels = ui.target.find("." + ui.panel_class );
     }
+
     function init( ui, hash ){
         ui.setOptions(defaults , typeof hash === "object" ? hash : {});
-        ui.target = createHTML(ui.data, ui)//创建一个ui实体
+        if(typeof ui.data_expr === "string" && ui.data_expr.length > 1){
+            ui.target = $(ui.data_expr);//选中页面上类似结构的HTML作为ui实体
+        }else{
+            ui.target = createHTML(ui.data, ui)//创建一个ui实体
+        }
         delete ui.data;//考虑有时它的HTML结构会很庞大，没有必要储存它
         reset(ui);//重新设置triggers与panels集合
         ui.panels.hide();//隐藏所有panels
@@ -36,21 +43,23 @@ $.define("switchable","more/uibase,event,attr,fx",function(Widget){
             ui.active(index, ui.active_callback, e);
         });
 
-        // autoplay_callback
+
         //如果pause_over_panel为true，则移到控件上方停止切换，移走恢复
         if(ui.pause_over_panel){
             ui.target.bind( "mouseenter", function( e ){
-                ui.autoplay = false;
-                ui.panels.stop(1,1);//立即停步所有动画，并让它们处于完成状态
-                clearTimeout(ui.timeoutID)
+                ui.pause();
                 $.log("停止切换");
             }).bind("mouseleave", function( e ){
                 if(!ui.autoplay){
-                    ui.autoplay = true
+                    ui.autoplay = true;
+                    ui.play()
                     $.log("恢复切换");
-                    ui.active(++ui.current_index);
                 }
             });
+        }
+        // autoplay_callback
+        if(ui.autoplay){
+            ui.play()
         }
 
     }
@@ -64,19 +73,27 @@ $.define("switchable","more/uibase,event,attr,fx",function(Widget){
             }
             ui.current_index = i;
             if( !reference.hasClass( active ) ){
+                //    ui.panels.stop(1,1);
                 //找到当前处于激活状态的trigger与panel，去掉它们的相应类名
                 var el = ui.target.find("."+active).removeClass( active )
                 //收起原来展开的面板
-                ui.panels.eq( ui.triggers.index( el ) ).removeClass( active ).slideUp(500);
+                ui.panels.eq( ui.triggers.index( el ) ).removeClass( active )[ui.switch_out]( 500 );
                 //将目标trigger与panel变为激活状态，并展开面板
                 reference.addClass( active );
-                ui.panels.eq(i).addClass(active).slideDown(500, function(){
-                    ui.timeoutID = setTimeout(function(){//如果开启了自动轮播功能
-                        ui.autoplay && ui.active(++ui.current_index);
-                    }, ui.delay);
-                });
+                ui.panels.eq(i).addClass(active)[ui.switch_in]( 500 );
                 callback && callback.call( e ? e.target : ui.target[0], e || ui);
             }
+        },
+ 
+        pause: function(){
+            this.autoplay = false;
+            clearInterval(this.timeoutID)
+        },
+        play: function(){
+            var ui = this;
+            ui.timeoutID = setInterval(function(){//如果开启了自动轮播功能
+                ui.autoplay && ui.active(++ui.current_index);
+            }, ui.delay)
         },
         //添加一组新的trigger与panel，index为它的插入位置
         add: function(index, trigger, panel ){
@@ -97,7 +114,7 @@ $.define("switchable","more/uibase,event,attr,fx",function(Widget){
         }
     });
 
-    $.fn.accordion = Widget.create("accordion", Accordion, init )
+    $.fn.accordion = Widget.create("switchable", Accordion, init )
 
 });
  //http://www.welefen.com/user-define-rich-content-filter-class.html
