e3c5ac094a9f2d2db223de872ea5830d8987cff4
RubyLouvre <cheng19840218@gmail.com>
2012-04-05 08:31:24 +0800

    修正mergeData BUG, 让$.data能获取HTML5 data-*

--- src/data.js
+++ src/data.js
@@ -3,20 +3,42 @@
 //==================================================
 $.define("data", "lang", function(){
     //$.log("已加载data模块");
-    var remitter = /object|function/
+    var remitter = /object|function/, rbrace = /^(?:\{.*\}|\[.*\])$/;
     $.mix( $, {
-        memcache:{},
+        _db:{},
         // 读写数据
         data : function( target, name, data, pvt ) {
             if(target && remitter.test(typeof target)){//只处理HTML节点与普通对象
-                var id = target.uniqueNumber || (target.uniqueNumber = $.uuid++);
+                var id = $.getUid(target), isEl = target.nodeType === 1
                 if(name === "@uuid"){
                     return id;
                 }
-                var memcache = target.nodeType === 1 ? $.memcache: target;
-                var table = memcache[ "@data_"+id ] || (memcache[ "@data_"+id ] = {});
+                var database = isEl ? $._db: target,
+                table = database[ "@data_"+id ] || (database[ "@data_"+id ] = {}),
+                _table =  table.data || (table.data = {});
+                if(isEl && !table.parsedAttrs){
+                    var attrs = target.attributes;
+                    //将HTML5单一的字符串数据转化为mass多元化的数据
+                    for ( var i = 0, attr, val; attr = attrs[i++];) {
+                        name = attr.name, val = attr.value
+                        if ( name.indexOf( "data-" ) === 0 && typeof  value == "string" ) {
+                            try {
+                                data = val === "true" ? true :
+                                val === "false" ? false :
+                                val === "null" ? null :
+                                isFinite( val ) ? +val :
+                                rbrace.test( val ) ? $.parseJSON( val ) :
+                                val;
+                            } catch( e ) {
+                                val = undefined
+                            }
+                            _table[ name + id ] = val
+                        }
+                    }
+                    table.parsedAttrs = true;
+                }
                 if ( !pvt ) {
-                    table = table.data || (table.data = {});
+                    table = _table;
                 }
                 var getByName = typeof name === "string";
                 if ( name && typeof name == "object" ) {
@@ -24,7 +46,7 @@ $.define("data", "lang", function(){
                 }else if(getByName && data !== void 0){
                     table[ name ] = data;
                 }
-                return getByName ? table[ name ] : table;
+                return getByName ? table[ name + id ] || table[ name ] : table;
             }
         },//仅内部调用
         _data:function(target,name,data){
@@ -33,12 +55,13 @@ $.define("data", "lang", function(){
         //移除数据
         removeData : function(target, name, pvt){
             if(target && remitter.test(typeof target)){
-                var id = target.uniqueNumber;
+                var id =  $.getUid(target);
                 if (  !id ) {
                     return;
                 }
-                var memcache = target.nodeType === 1  ? $.memcache : target;
-                var table = memcache["@data_"+id], clear = 1, ret = typeof name == "string" ;
+                var  clear = 1, ret = typeof name == "string",
+                database = target.nodeType === 1  ? $._db : target,
+                table = database["@data_"+id] ;
                 if ( table && ret ) {
                     if(!pvt){
                         table = table.data
@@ -47,7 +70,7 @@ $.define("data", "lang", function(){
                         ret = table[ name ];
                         delete table[ name ];
                     }
-                    var cache = memcache["@data_"+id];
+                    var cache = database["@data_"+id];
                         loop:
                         for(var key in cache){
                             if(key == "data"){
@@ -62,7 +85,7 @@ $.define("data", "lang", function(){
                         }
                 }
                 if(clear){
-                    delete memcache["@data_"+id];
+                    delete database["@data_"+id];
                 }
                 return ret;
             }
@@ -71,13 +94,12 @@ $.define("data", "lang", function(){
         mergeData:function(neo, src){
             var srcData = $._data(src), neoData = $._data(neo), events = srcData.events;
             if(srcData && neoData){
-                $.Object.merge(neoData, srcData);
+                $.Object.merge( neoData, srcData );
                 if(events){
-                    delete neoData.handle;
+                    delete neoData.callback;
                     neoData.events = {};
                     for ( var type in events ) {
                         for (var i = 0, obj ; obj =  events[ type ][i++]; ) {
-                           // console.log(obj)
                             $.event.bind.call( neo, obj );
                         }
                     }
@@ -85,10 +107,12 @@ $.define("data", "lang", function(){
             }
         }
     });
-    
 });
 
-//2011.9.27 uniqueID改为uniqueNumber 简化data与removeData
-//2011.9.28 添加$._data处理内部数据
-//2011.10.21 强化mergeData，可以拷贝事件
-//2012.1.31 简化$.Object.merge的调用
+/**
+2011.9.27 uniqueID改为uniqueNumber 简化data与removeData
+2011.9.28 添加$._data处理内部数据
+2011.10.21 强化mergeData，可以拷贝事件
+2012.1.31 简化$.Object.merge的调用
+2012.4.5 修正mergeData BUG, 让$.data能获取HTML5 data-*
+*/
