021709a1c0d379fdfd0f4106bd31d102cd19be4a
RubyLouvre <cheng19840218@gmail.com>
2012-06-08 08:54:28 +0800

    数据共享,但策略自定

--- src/flow.js
+++ src/flow.js
@@ -3,22 +3,25 @@
 //==========================================
 $.define("flow", function(){
     //像mashup，这里抓一些数据，那里抓一些数据，看似不相关，但这些数据抓完后最后构成一个新页面。
-    function OperateFlow(){
+    function OperateFlow(){ //数据共享,但策略自定
         this.root = {};
+        this.uuid = $.getUid({})
         if(typeof arguments[1] == "function")
             this.bind.apply(this, arguments);
     }
+    var db = $.flowDB = {};
     OperateFlow.prototype = {
         constructor: OperateFlow,
         //names 可以为数组，用逗号作为分隔符的字符串
-        bind:function(names,callback,reload){
+        bind:function(names,callback,reloadAll){
             var  root = this.root, deps = {},args = [];
             (names +"").replace($.rword,function(name){
                 name = "__"+name;//处理toString与valueOf等属性
                 if(!root[name]){
                     root[name] ={
                         unfire : [callback],//正在等待解发的回调
-                        fired:[]//已经触发的回调
+                        fired: [],//已经触发的回调
+                        state : 0
                     }
                 }else{
                     root[name].unfire.unshift(callback)
@@ -30,7 +33,7 @@ $.define("flow", function(){
             });
             callback.deps = deps;
             callback.args = args;
-            callback.reload = !!reload;//默认每次重新加载
+            callback.reloadAll = !!reloadAll;//默认每次重新加载
         },
         unbind : function(array,fn){//$.multiUnind("aaa,bbb")
             if(/string|number|object/.test(typeof array) ){
@@ -62,31 +65,32 @@ $.define("flow", function(){
         },
         fire : function(name, args){
             var root = this.root, obj = root["__"+name], deps;
-           
             if(!obj )
                 return ;
             obj.ret = args;
-            obj.state = 2;
+            obj.state = 2;//标识此操作已完成
             var unfire = obj.unfire,fired = obj.fired;
                 loop:
                 for (var i = unfire.length,repeat, fn; fn = unfire[--i]; ) {
                     deps = fn.deps;
-                    for(var key in deps){
+                    for(var key in deps){//如果其依赖的其他操作都已完成
                         if(deps.hasOwnProperty(key) && root[key].state != 2 ){
+                            $.log(key)
                             continue loop;
                         }
                     }
-                    unfire.splice(i,1);
-                    fired.push(fn);
+                    unfire.splice(i,1)
+                    fired.push( fn );//从unfire数组中取出 ,放进fired数组中
                     repeat = true;
                 }
-            if(repeat){
+            if(repeat){ //为了谨慎起见再检测一遍
                 return this.fire(name, args);
-            }else{
+            }else{//执行fired数组中的回调
                 for (i = fired.length; fn = fired[--i]; ) {
                     if(fn.deps["__"+name]){//只处理相关的
+                        $.log(fn.reloadAll)
                         fn.apply(this,this._args(fn.args));
-                        if(fn.reload){//所有数据必须重新加载
+                        if(fn.reloadAll){//重新加载所有数据
                             fired.splice(i,1);
                             unfire.push(fn);
                             for(key in fn.deps){
@@ -98,7 +102,7 @@ $.define("flow", function(){
             }
         }
     }
-    $.flow  = function(names,callback,reload){//一个工厂方法
-        return new OperateFlow(names,callback,reload)
+    $.flow  = function(names,callback,reloadAll){//一个工厂方法
+        return new OperateFlow(names,callback,reloadAll)
     }
 })
--- src/test/flow.js
+++ src/test/flow.js
@@ -2,14 +2,35 @@ $.define("flow","flow,more/spec",function(){
     $.fixture('操作流模块-flow', {
         flow: function(id){
             var flow = new $.flow;
-            var callback = function(){
+            var callback = function(a,b,c){
+                $.log([].slice.call(arguments))
+                expect([].slice.call(arguments),id).same([10,20,30])//测试多路监听时收集的返回值
                 expect("此流程触发在aaa,bbb,ccc都被fire的情况下",id).eq("此流程触发在aaa,bbb,ccc都被fire的情况下")
             }
-            flow.bind("aaa,bbb,ccc",callback,true);
-            expect(callback.deps).same({ __aaa:1, __bbb:1, __ccc:1});
-            expect(callback.args).same(["__aaa", "__bbb", "__ccc"]);
-            expect(callback.reload).eq(true);
-            
+            var callback2 = function(a,b,c){
+                $.log([].slice.call(arguments))
+                expect("此流程触发在aaa,ddd都被fire的情况下",id).eq("此流程触发在aaa,ddd都被fire的情况下")
+            }
+            flow.bind("aaa,bbb,ccc",callback,false);
+            flow.bind("aaa,ddd",callback2,true);
+            // $.log(callback.deps)
+            expect(callback.deps).same({
+                __aaa:1,
+                __bbb:1,
+                __ccc:1
+            });
+            // $.log(callback.args)
+            expect(callback.args).same(["__aaa", "__bbb", "__ccc"]);//必须这三个事件都被fire才执行callback
+            // $.log(callback.reloadAll)
+            expect(callback.reloadAll).eq(true);
+            //  $.log(flow.root["__aaa"])
+            expect(flow.root["__aaa"].unfire.length).eq(2);//__aaa这个事件未绑定的回调已达两个
+            flow.fire("aaa",10)
+            flow.fire("bbb",20)
+            flow.fire("ccc",30);
+             flow.fire("aaa",40);
+              flow.fire("bbb",20)
+               flow.fire("ccc",20)
         }
     });
 })
