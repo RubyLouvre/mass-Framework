8031e9b43f9387c0eca176ef759ee2e5703cc491
RubyLouvre <cheng19840218@gmail.com>
2012-06-10 20:34:02 +0800

    修正第二分支的判定

--- src/more/watcher.js
+++ src/more/watcher.js
@@ -29,7 +29,7 @@ $.define("watcher","lang", function(){
                 }
             }
         };
-    }else if( $.isNative(Object.defineProperties ||  watcher.__defineGetter__ )){
+    }else if( Object.seal ||  watcher.__defineGetter__ ){
         //IE8也有Object.defineProperty，但不能用于普通JS对象。为了防止某些es5-shim库，它们对Object.defineProperty的修复是不完全
         //一定是原生支持才行
         watcher.watch = function(obj, prop, handler){
@@ -125,3 +125,111 @@ $.define("watcher","lang", function(){
 })
 //参考 http://stackoverflow.com/questions/1269633/watch-for-object-properties-changes-in-javascript
 //Detect Attribute Changes with jQuery The Problem http://darcyclarke.me/development/detect-attribute-changes-with-jquery/#
+/*
+  (function(watch, unwatch){
+                createWatcher = watch && unwatch ?
+                    // Andrea Giammarchi - Mit Style License
+                function(Object){
+                    var handlers = [];
+                    return {
+                        destroy:function(){
+                            handlers.forEach(function(prop){
+                                unwatch.call(this, prop);
+                            }, this);
+                            delete handlers;
+                        },
+                        watch:function(prop, handler){
+                            if(-1 === handlers.indexOf(prop))
+                                handlers.push(prop);
+                            watch.call(this, prop, function(prop, prevValue, newValue){
+                                return Object[prop] = handler.call(Object, prop, prevValue, newValue);
+                            });
+                        },
+                        unwatch:function(prop){
+                            var i = handlers.indexOf(prop);
+                            if(-1 !== i){
+                                unwatch.call(this, prop);
+                                handlers.splice(i, 1);
+                            };
+                        }
+                    }
+                }:(Object.prototype.__defineSetter__?
+                    function(Object){
+                    var handlers = [];
+                    return {
+                        destroy:function(){
+                            handlers.forEach(function(prop){
+                                delete this[prop];
+                            }, this);
+                            delete handlers;
+                        },
+                        watch:function(prop, handler){
+                            if(-1 === handlers.indexOf(prop))
+                                handlers.push(prop);
+                            if(!this.__lookupGetter__(prop))
+                                this.__defineGetter__(prop, function(){return Object[prop]});
+                            this.__defineSetter__(prop, function(newValue){
+                                Object[prop] = handler.call(Object, prop, Object[prop], newValue);
+                            });
+                        },
+                        unwatch:function(prop){
+                            var i = handlers.indexOf(prop);
+                            if(-1 !== i){
+                                delete this[prop];
+                                handlers.splice(i, 1);
+                            };
+                        }
+                    };
+                }:
+                    function(Object){
+                    function onpropertychange(){
+                        var prop = event.propertyName,
+                        newValue = empty[prop]
+                        prevValue = Object[prop],
+                        handler = handlers[prop];
+                        if(handler)
+                            attachEvent(detachEvent()[prop] = Object[prop] = handler.call(Object, prop, prevValue, newValue));
+                    };
+                    function attachEvent(){empty.attachEvent("onpropertychange", onpropertychange)};
+                    function detachEvent(){empty.detachEvent("onpropertychange", onpropertychange);return empty};
+                    var empty = document.createElement("empty"), handlers = {};
+                    empty.destroy = function(){
+                        detachEvent();
+                        empty.parentNode.removeChild(empty);
+                        empty = handlers = null;
+                    };
+                    empty.watch = function(prop, handler){handlers[prop] = handler};
+                    empty.unwatch = function(prop){delete handlers[prop]};
+                    attachEvent();
+                    return (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(empty);
+                }
+            )
+                ;
+            })(Object.prototype.watch, Object.prototype.unwatch);
+
+            var // original object
+            man = {},
+            // create its watcher
+            manWatcher = createWatcher(man);
+
+            manWatcher.watch("name", function(propertyName, oldValue, newValue){
+                return "Mr " + newValue;
+            });
+
+            // assign name property
+            manWatcher.name = "Andrea";
+
+            // retrieve original object property
+            alert(man.name); // Mr Andrea
+            var obj = {},
+            watcher = createWatcher(obj);
+
+            watcher.watch("test", function(prop, oldValue, newValue){
+                return oldValue === undefined ? newValue : oldValue;
+            });
+
+            watcher.test = "one assignment";
+            watcher.test = "never again";
+
+            alert(obj.test); // one assignment
+            */
