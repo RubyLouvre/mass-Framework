96c0f9185c634498953fd64e776c06efc850b85f
RubyLouvre <cheng19840218@gmail.com>
2012-07-22 03:00:45 +0800

    class 类工厂升级到v10

--- src/class.js
+++ src/class.js
@@ -17,27 +17,16 @@ $.define("class", "lang",function(){
         });
         return klass;
     }
-    var defineProperties = false;
-    // 指定了get与set不能指定writable与value
-    var hiddenProperty = {
-        configurable: false,//防止被删除
-        enumerable: true,//防止被遍历
-        writable: false//防止被修改
-    }
-    try{
-        Object.defineProperties($,{
-            "@bind": hiddenProperty,
-            "@path": hiddenProperty
-        });
-        defineProperties = $.support.defineProperties = true;
-    }catch(e){}
+
     $.mutators = {
         inherit : function( parent,init ) {
+            var bridge = function() { }
             if( typeof parent == "function"){
                 for(var i in parent){//继承类成员
                     this[i] = parent[i];
                 }
-                this.prototype = Object.create(parent.prototype) ;//继承原型成员
+                bridge.prototype = parent.prototype;
+                this.prototype = new bridge ;//继承原型成员
                 this._super = parent;//指定父类
             }
             this._init = (this._init || []).concat();
@@ -45,10 +34,9 @@ $.define("class", "lang",function(){
                 this._init.push(init);
             }
             this.toString = function(){
-                return (init || $.noop) + "";
+                return (init || bridge) + ""
             }
             var proto = this.prototype;
-            proto.klass = this;
             proto.setOptions = function(){
                 var first = arguments[0];
                 if( typeof first === "string" ){
@@ -60,69 +48,46 @@ $.define("class", "lang",function(){
                 $.Object.merge.apply(null,arguments);
                 return this;
             }
-            if(defineProperties){
-                Object.defineProperties(proto, {
-                    _init: hiddenProperty,
-                    setOptions: hiddenProperty,
-                    klass: hiddenProperty
-                })
-            }
             return proto.constructor = this;
         },
-        implement: function(){
-            var target = this.prototype, parent = this._super, reg = rconst, definition = {}, accessor
+        implement:function(){
+            var target = this.prototype, parent = this._super, reg = rconst;
             for(var i = 0, module; module = arguments[i++]; ){
                 module = typeof module === "function" ? new module :module;
                 Object.keys(module).forEach(function(name){
                     if( !reg.test(name) ){
-                        var prop =  target[name] = module[name];
-                        if($.isPlainObject(prop) && (  accessor = ($.isFunction( prop.get ) || $.isFunction( prop.set )) || "value" in prop  ) ){
-                            if(defineProperties){
-                                definition[name] = prop;
-                                if (!('enumerable' in prop))
-                                    prop.enumerable = true;
-                                if(accessor){
-                                    delete prop.value;//指定了访问器就不能指定这两个属性
-                                    delete prop.writable
-                                }else if (!('writable' in prop)){
-                                     prop.writable =  true;
-                                }
-                            }else if("value" in prop){//如果不支持,直接赋值
-                                target[name] = prop.value;
-                            }
-                        }
-                        if(typeof prop == "function"){
-                            target[name] = (function(){
-                                var _super = function() {
-                                    return parent.prototype[ name ].apply( this, arguments );
-                                };
-                                var _superApply = function( args ) {
-                                    return parent.prototype[ name ].apply( this, args );
-                                };
-                                return function() {
-                                    var __super = this._super,
-                                    __superApply = this._superApply,
-                                    ret;
-
-                                    this._super = _super;
-                                    this._superApply = _superApply;
-
-                                    ret = prop.apply( this, arguments );
-
-                                    this._super = __super;
-                                    this._superApply = __superApply;
-
-                                    return ret;
-                                };
-                            })();
-                            target[name].toString = function(){
-                                return prop +"";
-                            }
-                        }
+                        var prop = target[name] = module[name];
+//                        if(typeof prop == "function"){
+//                            target[name] = (function(){
+//                                var _super = function() {
+//                                    return parent.prototype[ name ].apply( this, arguments );
+//                                };
+//                                var _superApply = function( args ) {
+//                                    return parent.prototype[ name ].apply( this, args );
+//                                };
+//                                return function() {
+//                                    var __super = this._super,
+//                                    __superApply = this._superApply,
+//                                    ret;
+//
+//                                    this._super = _super;
+//                                    this._superApply = _superApply;
+//
+//                                    ret = prop.apply( this, arguments );
+//
+//                                    this._super = __super;
+//                                    this._superApply = __superApply;
+//
+//                                    return ret;
+//                                };
+//                            })();
+//                            target[name].toString = function(){
+//                                return prop +"";
+//                            }
+//                        }
                     }
                 }, this );
             }
-            defineProperties && Object.defineProperties(this.prototype, definition)
             return this;
         },
         extend: function(){//扩展类成员
@@ -167,5 +132,4 @@ fix 子类实例不是父类的实例的bug
 2012.1.29 修正setOptions中$.Object.merge方法的调用方式
 2012.2.25 改进setOptions，可以指定在this上扩展还是在this.XXX上扩展
 2012.2.26 重新实现方法链，抛弃arguments.callee.caller   v8
-2012.5.31 添加数据描述符的应用 v9
 */
