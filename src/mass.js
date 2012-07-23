7ac11df4cd32b03b263112bdfccf7566284a5bb6
RubyLouvre <cheng19840218@gmail.com>
2012-07-23 15:11:16 +0800

    优化$["@debug"]

--- src/mass.js
+++ src/mass.js
@@ -88,7 +88,8 @@ void function( global, DOC ){
             node = scripts[ scripts.length - 1 ];//FF下可以使用DOC.currentScript
             url = node.hasAttribute ?  node.src : node.getAttribute( 'src', 4 );
             $["@name"] = node.getAttribute("namespace") || "$"
-            $["@debug"] = !!node.getAttribute("debug");
+            var str = node.getAttribute("debug")
+            $["@debug"] = str == 'true' || str == '1';
             return url.substr( 0, url.lastIndexOf('/') );
         })(),
         /**
