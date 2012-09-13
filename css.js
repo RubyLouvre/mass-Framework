3aa53e02a16e41a8840b2cdb7253c11de8a17f53
RubyLouvre <cheng19840218@gmail.com>
2012-09-14 07:04:20 +0800

    FIX $.fn.offset BUG!

--- css.js
+++ css.js
@@ -2,6 +2,7 @@
 // 样式操作模块 by 司徒正美
 //=========================================
 define( "css", !!top.getComputedStyle ? ["$node"] : ["$node","$css_fix"] , function(){
+    try{
     //$.log( "已加载css模块" );
     var adapter = $.cssAdapter = $.cssAdapter || {}
     var rrelNum = /^([\-+])=([\-+.\de]+)/
@@ -408,6 +409,7 @@ define( "css", !!top.getComputedStyle ? ["$node"] : ["$node","$css_fix"] , funct
         }
     }
     $.fn.offset = function(options){//取得第一个元素位于页面的坐标
+        throw "ccccccccccccc"
         if ( arguments.length ) {
             return (!options || ( !isFinite(options.top) && !isFinite(options.left) ) ) ?  this :
             this.each(function() {
@@ -566,7 +568,10 @@ define( "css", !!top.getComputedStyle ? ["$node"] : ["$node","$css_fix"] , funct
     function getWindow( node ) {
         return $.type(node,"Window") ?   node : node.nodeType === 9 ? node.defaultView || node.parentWindow : false;
     } ;
-
+    }catch(e){
+        throw module.id;
+        throw e
+    }
 });
 /**
 2011.9.5将cssName改为隋性函数,修正msTransform Bug
