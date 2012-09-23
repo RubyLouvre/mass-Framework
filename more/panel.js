bb5f0fe976cae13f87ff9fe2cb4f7bdae5dd1e7c
RubyLouvre <cheng19840218@gmail.com>
2012-09-23 19:37:12 +0800

    添加panel UI！

--- more/panel.js
+++ more/panel.js
@@ -1,23 +1,61 @@
-define("panel",["$node","$event","$css"], function(){
-    $.ui = $.ui || {};
-    $.ui.Panel = $.factory({
-        init: function(opts){
-
-            var html = '<div class="panel_wrap"><div class="panel_header"><div class="panel_title"></div><span class="panel_closer"></span></div><div class="panel_body"></div></div>'
-            var parent = opts.parent || "body";
-            var ui = $(html).appendTo(parent);
-            
-            ui.find(".panel_title").html( opts.title || "")
-            ui.find(".panel_body").html( opts.body || "");
-
-            ui.width( opts.width || 400 );
-            ui.height( opts.height || 200 );
-            ui.css( opts.css || {} );
-            //closer为布尔
-            if(opts.closer == false){
-                ui.find(".panel_closer").hide()
+define('panel',[
+    '$node',
+    '$event',
+    '$css',
+    '$flow',
+    "./panel.css",
+    "./avalon"
+    ], function(){
+        $.ui = $.ui||{}
+        var defaults = {
+            show        : true,
+            showHead    : true,
+            showFoot    : true,
+            closeAble   : true,
+            parent      : 'body',
+            content     : {
+                title       : 'title',
+                body        : 'body',
+                foot        : ''
+            },
+            css         : {
+                width       : 400,
+                height      : 200
             }
-        }
-    })
+        };
+        $.ui.Panel = $.factory({
+            inherit: $.Flow,
+            init: function(opts) {
+                this.setOptions ("data", defaults, opts );
+                var data = this.data;
+                var html =
+                '<div class="panel_wrap" bind="display:show">'+
+                '    <div class="panel_head" bind="if:showHead">'+
+                '        <div class="panel_title" bind="html:content.title"></div>'+
+                '        <span class="panel_closer" bind="display:closeAble"></span>'+
+                '     </div>'+
+                '     <div class="panel_body" bind="html:content.body"></div>'+
+                '     <div class="panel_foot" bind="if:showFoot,html:content.foot"></div>'+
+                '</div>'
+                this.ui = $(html)
+                .appendTo( data.parent )
+                .css     ( data.css )
+console.log(data)
+                var model =  $.ViewModel( data)
+                $.View(model,this.ui[0])
 
-})
+
+
+               // self.show();
+            },
+            show : function() {
+
+            },
+            hide : function() {
+
+            },
+            set : function( keyChain, val ) {//每改一个属性就重刷整个视图，因此不能容纳子控件,除非我们多做一些额外工作
+        
+            }
+        });
+    })
