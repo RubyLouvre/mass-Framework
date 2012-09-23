65de8e88abae5ee671d90074b200c6881ef2004e
RubyLouvre <cheng19840218@gmail.com>
2012-09-23 20:34:33 +0800

    更新样式

--- more/panel.js
+++ more/panel.js
@@ -25,27 +25,29 @@ define('panel',[
             init: function(opts) {
                 this.setOptions ("data", defaults, opts );
                 var data = this.data;
-                var html = //不要使用换行符,这在压缩时很容易出现问题
+                this.tmpl  = //不要使用换行符,这在压缩时很容易出现问题
                 '<div class="panel_wrap" bind="display:show">'+
                 '    <div class="panel_head" bind="if:showHead">'+
                 '        <div class="panel_title" bind="html:content.title"></div>'+
-                '        <span class="panel_closer" bind="display:closeAble"></span>'+
+                '        <div class="panel_tool">'+
+                '            <span class="panel_tool_close" bind="display:closeAble"></span>'+
+                '        </div>'+
                 '     </div>'+
                 '     <div class="panel_body" bind="html:content.body"></div>'+
                 '     <div class="panel_foot" bind="if:showFoot,html:content.foot"></div>'+
                 '</div>'
-                this.ui = $(html)
+                this.ui = $(this.tmpl)
                 .appendTo( data.parent )
                 .css     ( data.css )
                 this.VM =  $.ViewModel( data);
                 $.View(this.VM, this.ui[0]);
             },
             show : function() {
-                $.log("show")
+                $.log("show",7)
                 this.VM.show(true)
             },
             hide : function() {
-                $.log("hide")
+                $.log("hide",7)
                 this.VM.show(false)
             },
             set : function( name, val ) {
@@ -62,6 +64,7 @@ define('panel',[
         });
     })
     /*
+     *后继计划是将工具栏做成一个独立的UI,有放大,缩小,关闭等按钮
      <script>
             $.require("ready,./more/panel", function(){
                 var model = new $.ui.Panel()
