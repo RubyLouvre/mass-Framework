f45e48a9f5ce86316f3d3035ecb14b42cd2808fe
RubyLouvre <cheng19840218@gmail.com>
2012-10-23 18:30:28 +0800

    hashchange

--- more/hashchange.js
+++ more/hashchange.js
@@ -10,6 +10,12 @@ define("hashchange", ["$event"], function(){
     $.fn[ hashchange ].delay = 50;
     if(!supportHashChange){
         $.log("不支持hashchange，使用iframe加定时器模拟")
+        var iframe, timeoutID, html = '<!doctype html><html><body>#{0}</body></html>'
+        
+        if( $.fn[ hashchange ].domain){
+            html = html.replace("<body>","<script>document.domain ="+ 
+                $.fn[ hashchange ].domain +"</script><body>" )
+        }
         // IE6直接用location.hash取hash，可能会取少一部分内容
         // 比如 http://www.cnblogs.com/rubylouvre#stream/xxxxx?lang=zh_c
         // ie6 => location.hash = #stream/xxxxx
@@ -18,50 +24,56 @@ define("hashchange", ["$event"], function(){
         // 又比如 http://www.cnblogs.com/rubylouvre/#!/home/q={%22thedate%22:%2220121010~20121010%22}
         // firefox 15 => #!/home/q={"thedate":"20121010~20121010"}
         // 其他浏览器 => #!/home/q={%22thedate%22:%2220121010~20121010%22}
-        function getHash ( ) {
-            return '#' + DOC.URL.replace( /^[^#]*#?(.*)$/, '$1' );
+        function getHash ( url) {//用于取得当前窗口或iframe窗口的hash值
+            url = url || DOC.URL
+            return '#' + url.replace( /^[^#]*#?(.*)$/, '$1' );
         }
-        //将主窗口的location.href加工一下，赋给iframe中的location
-        function setHash( hash){
-            if(iframe){
-                var doc = iframe.document
+        function getHistory(){
+            return getHash(iframe.location);
+        }
+        function setHistory(hash, history_hash){
+            var doc = iframe.document;
+            if (  hash !== history_hash ) {//只有当新hash不等于iframe中的hash才重写
                 //用于产生历史
-                doc.open();
-                doc.write($.format(html, hash));
-                doc.close();
-                iframe.location  = DOC.URL.replace( /#.*/, '' ) + hash;
-
+                try{
+                    doc.open();
+                    doc.write($.format(html, hash));
+                    doc.close();
+                }catch(e){
+                    clearInterval(timeoutID)
+                }
             }
         }
-        var iframe, cur = getHash(), now, timeoutID, html = '<!doctype html><html><body>#{0}</body></html>'
+        var last_hash = getHash(), history_hash, hash = "#";
         $.eventAdapter[ hashchange ] = {
             setup: function(desc) {
                 $.require("ready", function(){
                     if (!iframe) {
                         //创建一个隐藏的iframe，使用这博文提供的技术 http://www.paciellogroup.com/blog/?p=604.
                         //iframe是直接加载父页面，为了防止死循环，在DOM树未建完之前就擦入新的内容
-                        iframe = $('<iframe tabindex="-1" '+ DOC.URL+ 'hidden style="display:none" widht=0 height=0 title="empty" />')
-                        .one( 'load', function(){
-                            timeoutID = setInterval(poll, $.fn[ hashchange ].delay)
-                            }).appendTo( "body" )[0].contentWindow;
+                        //style="display:none" widht=0 height=0 title="empty" 
+                        var el = $('<iframe tabindex="-1" style="display:none" widht=0 height=0 title="empty" />').appendTo( document.body )[0], fn
+                        iframe = el.contentWindow
+                        $.bind(el, "load",fn = function(){
+                            $.unbind(el, "load", fn)
+                            var doc = iframe.document
+                            doc.open();
+                            doc.write($.format(html, hash))
+                            doc.close();
+                            timeoutID = setInterval(poll,  $.fn[ hashchange ].delay)
+                        });
                         function poll() {
-                            var now = getHash();
-                            if(cur !== now){
-                                setHash(cur = now )
+                            var hash = getHash(),//取得主窗口中的hash
+                            history_hash = iframe.document.body.innerText;//取得现在iframe中的hash
+                            if(hash !== last_hash){//如果是主窗口的hash发生变化
+                                setHistory(last_hash = hash, history_hash )
                                 $(desc.currentTarget).fire(hashchange)
+                            }else if(history_hash !== last_hash){//如果按下回退键，
+                                location.href = location.href.replace( /#.*/, '' ) + history_hash;
                             }
                         }
                     }
-                    void function clear(){//擦入新的内容
-                        try {
-                            var doc = iframe.document
-                            doc.open();
-                            doc.write($.format(html, cur))
-                            doc.close();
-                        } catch(e) {
-                            setTimeout( clear, 16 );
-                        }
-                    }()
+                   
                 });
             },
             teardown: function(){
@@ -77,3 +89,19 @@ define("hashchange", ["$event"], function(){
 })
 
 
+/**
+* 
+      <script type="html">
+            $(function(){
+            var el = $("#show_result")
+            document.onclick = function() {
+            var t = "#" + Math.random();
+            location.hash = t
+            }
+            $(window).hashchange(function() {
+            el.html(location.href)
+            })
+            })
+        </script>
+* 
+*/
