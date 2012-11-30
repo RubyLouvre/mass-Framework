400817f854b874d2b176f7746902a9bddc10c596
RubyLouvre <cheng19840218@gmail.com>
2012-11-30 17:13:50 +0800

    update menu组件

--- menu.js
+++ menu.js
@@ -11,27 +11,22 @@ define("menu", ["node","attr","css","event","fx"], function($){
                 $('>a:first-child',this).addClass(c.anchorClass).append($arrow);
             }
         })
-
+        var id 
         this.find("li").mouseenter(function(){
-            var $$ = $(this)
+            var $$ = $(this);
+            clearTimeout(id);
             $$.addClass(o.hoverClass).find("li."+o.hoverClass).removeClass(o.hoverClass)
             $$.siblings().removeClass(o.hoverClass)
-            if(o.animation){
-                var fx = o.animation;
-                fx.after = function(){
-                    o.onShow($$[0])       
-                }
-                $$.find(">ul").fx(o.duration,fx)
-            }else{            
-                o.onShow(this)     
-            }
-          
+            o.onShow(this)     
         }).mouseleave(function(){
-            $(this).removeClass(o.hoverClass)
-            o.onHide(this)
+            clearTimeout(id);
+            var $$ = $(this);
+            id = setTimeout(function(){
+                $$.removeClass(o.hoverClass)
+                o.onHide($$[0])
+            },o.delay)
         })
 
-       
     };
 
     var sf = $.fn.superfish;
@@ -50,11 +45,7 @@ define("menu", ["node","attr","css","event","fx"], function($){
     };
     sf.defaults = {
         hoverClass	: 'sfHover',
-        delay		: 300,
-        animation	: {
-            opacity:'show'
-        },
-        duration	:700,
+        delay		: 500,
         autoArrows	: true,
         dropShadows     : true,
         disableHI	: false,		// true disables hoverIntent detection
