87ed55027b534cf3832d25952e4e4a3420e1cca6
RubyLouvre <cheng19840218@gmail.com>
2013-01-13 14:32:47 +0800

    还是决定内置scrollParent

--- css.js
+++ css.js
@@ -3,9 +3,9 @@
 //=========================================
 define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
     var adapter = $.cssHooks || ($.cssHooks = {}),
-        rrelNum = /^([\-+])=([\-+.\de]+)/,
-        rnumnonpx = /^-?(?:\d*\.)?\d+(?!px)[^\d\s]+$/i,
-        cssTransform = $.cssName("transform");
+    rrelNum = /^([\-+])=([\-+.\de]+)/,
+    rnumnonpx = /^-?(?:\d*\.)?\d+(?!px)[^\d\s]+$/i,
+    cssTransform = $.cssName("transform");
     adapter["_default:set"] = function(node, name, value) {
         node.style[name] = value;
     }
@@ -43,26 +43,26 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
     }
 
     adapter["zIndex:get"] = function(node) {
-            while(node.nodeType !== 9) {
-                //即使元素定位了，但如果zindex设置为"aaa"这样的无效值，浏览器都会返回auto;
-                //如果没有指定zindex值，IE会返回数字0，其他返回auto
-                var position = getter(node, "position") || "static"; //getter = adapter[ "_default:get" ]
-                if(position !== "static") {
-                    // <div style="z-index: -10;"><div style="z-index: 0;"></div></div>
-                    var value = parseInt(getter(node, "zIndex"), 10);
-                    if(!isNaN(value) && value !== 0) {
-                        return value;
-                    }
+        while(node.nodeType !== 9) {
+            //即使元素定位了，但如果zindex设置为"aaa"这样的无效值，浏览器都会返回auto;
+            //如果没有指定zindex值，IE会返回数字0，其他返回auto
+            var position = getter(node, "position") || "static"; //getter = adapter[ "_default:get" ]
+            if(position !== "static") {
+                // <div style="z-index: -10;"><div style="z-index: 0;"></div></div>
+                var value = parseInt(getter(node, "zIndex"), 10);
+                if(!isNaN(value) && value !== 0) {
+                    return value;
                 }
-                node = node.parentNode;
             }
-            return 0;
+            node = node.parentNode;
         }
+        return 0;
+    }
 
-        // 获取CSS3变形中的角度
-        adapter["rotate:get"] = function(node) {
-            return $._data(node, 'rotate') || 0;
-        }
+    // 获取CSS3变形中的角度
+    adapter["rotate:get"] = function(node) {
+        return $._data(node, 'rotate') || 0;
+    }
     if(cssTransform) {
         adapter["rotate:set"] = function(node, name, value) {
             $._data(node, 'rotate', value);
@@ -72,11 +72,11 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
 
     var supportBoxSizing = $.cssName("box-sizing")
     adapter["boxSizing:get"] = function(node, name) {
-            return supportBoxSizing ? getter(node, name) : document.compatMode == "BackCompat" ? "border-box" : "content-box"
-        }
+        return supportBoxSizing ? getter(node, name) : document.compatMode == "BackCompat" ? "border-box" : "content-box"
+    }
 
-        //这里的属性不需要自行添加px
-        $.cssNumber = $.oneObject("fontSizeAdjust,fontWeight,lineHeight,opacity,orphans,widows,zIndex,zoom,rotate");
+    //这里的属性不需要自行添加px
+    $.cssNumber = $.oneObject("fontSizeAdjust,fontWeight,lineHeight,opacity,orphans,widows,zIndex,zoom,rotate");
     $.css = function(node, name, value) {
         if(node.style) { //注意string经过call之后，变成String伪对象，不能简单用typeof来检测
             var prop = $.String.camelize(name)
@@ -165,9 +165,9 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
     //=========================　处理　width, height, innerWidth, innerHeight, outerWidth, outerHeight　========
     "Height,Width".replace($.rword, function(name) {
         var lower = name.toLowerCase(),
-            clientProp = "client" + name,
-            scrollProp = "scroll" + name,
-            offsetProp = "offset" + name;
+        clientProp = "client" + name,
+        scrollProp = "scroll" + name,
+        offsetProp = "offset" + name;
         $.cssHooks[lower + ":get"] = function(node) {
             return getWH(node, name, 0) + "px"; //添加相应适配器
         }
@@ -189,7 +189,7 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
                         //IE 标准模式 : html.scrollHeight> body.scrollHeight
                         //IE 怪异模式 : html.scrollHeight 最大等于可视窗口多一点？
                         return Math.max(
-                        node.body[scrollProp], doc[scrollProp], node.body[offsetProp], doc[offsetProp], doc[clientProp]);
+                            node.body[scrollProp], doc[scrollProp], node.body[offsetProp], doc[offsetProp], doc[clientProp]);
                     } else if(size === void 0) {
                         return getWH(node, name, num)
                     } else {
@@ -202,22 +202,22 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
     });
     //=========================　生成　show hide toggle　=========================
     var cacheDisplay = $.oneObject("a,abbr,b,span,strong,em,font,i,img,kbd", "inline"),
-        blocks = $.oneObject("div,h1,h2,h3,h4,h5,h6,section,p", "block"),
-        sandbox, sandboxDoc
-        $.callSandbox = function(parent, callback) {
-            if(!sandbox) {
-                sandbox = document.createElement("iframe");
-                sandbox.frameBorder = sandbox.width = sandbox.height = 0;
-            }
-            parent.appendChild(sandbox);
-            if(!sandboxDoc || !sandbox.createElement) {
-                sandboxDoc = (sandbox.contentWindow || sandbox.contentDocument).document;
-                sandboxDoc.write("<!doctype html><html><body>");
-                sandboxDoc.close();
-            }
-            callback(sandboxDoc);
-            parent.removeChild(sandbox);
+    blocks = $.oneObject("div,h1,h2,h3,h4,h5,h6,section,p", "block"),
+    sandbox, sandboxDoc
+    $.callSandbox = function(parent, callback) {
+        if(!sandbox) {
+            sandbox = document.createElement("iframe");
+            sandbox.frameBorder = sandbox.width = sandbox.height = 0;
         }
+        parent.appendChild(sandbox);
+        if(!sandboxDoc || !sandbox.createElement) {
+            sandboxDoc = (sandbox.contentWindow || sandbox.contentDocument).document;
+            sandboxDoc.write("<!doctype html><html><body>");
+            sandboxDoc.close();
+        }
+        callback(sandboxDoc);
+        parent.removeChild(sandbox);
+    }
 
     $.mix(cacheDisplay, blocks);
     $.parseDisplay = function(nodeName) {
@@ -239,9 +239,9 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
 
     function toggelDisplay(nodes, show) {
         var elem, values = [],
-            status = [],
-            index = 0,
-            length = nodes.length;
+        status = [],
+        index = 0,
+        length = nodes.length;
         //由于传入的元素们可能存在包含关系，因此分开两个循环来处理，第一个循环用于取得当前值或默认值
         for(; index < length; index++) {
             elem = nodes[index];
@@ -288,13 +288,13 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
                 node.style.position = "relative";
             }
             var curElem = $(node),
-                curOffset = curElem.offset(),
-                curCSSTop = $.css(node, "top"),
-                curCSSLeft = $.css(node, "left"),
-                calculatePosition = (position === "absolute" || position === "fixed") && [curCSSTop, curCSSLeft].indexOf("auto") > -1,
-                props = {},
-                curPosition = {},
-                curTop, curLeft;
+            curOffset = curElem.offset(),
+            curCSSTop = $.css(node, "top"),
+            curCSSLeft = $.css(node, "left"),
+            calculatePosition = (position === "absolute" || position === "fixed") && [curCSSTop, curCSSLeft].indexOf("auto") > -1,
+            props = {},
+            curPosition = {},
+            curTop, curLeft;
             if(calculatePosition) {
                 curPosition = curElem.position();
                 curTop = curPosition.top;
@@ -323,11 +323,11 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
         }
 
         var node = this[0],
-            doc = node && node.ownerDocument,
-            pos = {
-                left: 0,
-                top: 0
-            };
+        doc = node && node.ownerDocument,
+        pos = {
+            left: 0,
+            top: 0
+        };
         if(!doc) {
             return pos;
         }
@@ -335,12 +335,12 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
         //我们可以通过getBoundingClientRect来获得元素相对于client的rect.
         //http://msdn.microsoft.com/en-us/library/ms536433.aspx
         var box = node.getBoundingClientRect(),
-            win = getWindow(doc),
-            root = (navigator.vendor || doc.compatMode == "BackCompat") ? doc.body : doc.documentElement,
-            clientTop = root.clientTop >> 0,
-            clientLeft = root.clientLeft >> 0,
-            scrollTop = win.pageYOffset || root.scrollTop,
-            scrollLeft = win.pageXOffset || root.scrollLeft;
+        win = getWindow(doc),
+        root = (navigator.vendor || doc.compatMode == "BackCompat") ? doc.body : doc.documentElement,
+        clientTop = root.clientTop >> 0,
+        clientLeft = root.clientLeft >> 0,
+        scrollTop = win.pageYOffset || root.scrollTop,
+        scrollLeft = win.pageXOffset || root.scrollLeft;
         // 把滚动距离加到left,top中去。
         // IE一些版本中会自动为HTML元素加上2px的border，我们需要去掉它
         // http://msdn.microsoft.com/en-us/library/ms533564(VS.85).aspx
@@ -351,10 +351,10 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
     //=========================　处理　position　=========================
     $.fn.position = function() { //取得元素相对于其offsetParent的坐标
         var offset, offsetParent, node = this[0],
-            parentOffset = { //默认的offsetParent相对于视窗的距离
-                top: 0,
-                left: 0
-            }
+        parentOffset = { //默认的offsetParent相对于视窗的距离
+            top: 0,
+            left: 0
+        }
         if(!node || node.nodeType !== 1) {
             return
         }
@@ -387,7 +387,19 @@ define("css", top.getComputedStyle ? ["$node"] : ["$css_fix"], function($) {
             return el || document.documentElement;
         });
     }
-
+    $.fn.scrollParent = function() {
+        var scrollParent;
+        if ((window.VBArray && (/(static|relative)/).test(this.css('position'))) || (/absolute/).test(this.css('position'))) {
+            scrollParent = this.parents().filter(function() {
+                return (/(relative|absolute|fixed)/).test($.css(this,'position')) && (/(auto|scroll)/).test($.css(this,'overflow')+$.css(this,'overflow-y')+$.css(this,'overflow-x'));
+            }).eq(0);
+        } else {
+            scrollParent = this.parents().filter(function() {
+                return (/(auto|scroll)/).test($.css(this,'overflow')+$.css(this,'overflow-y')+$.css(this,'overflow-x'));
+            }).eq(0);
+        }
+        return (/fixed/).test(this.css('position')) || !scrollParent.length ? $(document) : scrollParent;
+    }
     //=========================　处理　scrollLeft scrollTop　=========================
     "scrollLeft_pageXOffset,scrollTop_pageYOffset".replace($.rmapper, function(_, method, prop) {
         $.fn[method] = function(val) {
