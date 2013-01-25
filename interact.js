fbca3eef06b7115bc198ac5d3857150c339d1d81
RubyLouvre <cheng19840218@gmail.com>
2013-01-25 12:15:52 +0800

    升级interact!

--- interact.js
+++ interact.js
@@ -62,8 +62,11 @@ define("interact",["$class"], function($){
     //多路监听，收集每个子步骤的执行结果，触发最终回调,解耦回调的深层嵌套
     $.Flow = $.factory({
         inherit: $.Observer,
-        init: function(){
-            this._fired = {};
+        init: function(timeout){
+            this._fired = {};//用于收集fire或order的参数(去掉第一个事件参数)
+            if(typeof timeout == "number"){
+                this.timeout = timeout;//用于order,时间限制
+            }
         },
         fire: function (type, args) {
             var calls = this._events, normal = 2, listeners, ev
@@ -129,6 +132,40 @@ define("interact",["$class"], function($){
             _assign.apply(this, arguments);
             return this;
         },
+        /**配合refresh使用,相当于一种特殊的fire,要求用户在fire时,按refresh的第一个参数绑定的顺序依次执行
+        require("interact",function($){
+            var flow = new $.Flow;
+            flow.refresh("aaa,bbb,ccc", function(){
+                $.log(Array.apply([],arguments))
+            });
+            flow.order("aaa",1);//没有反应
+            flow.order("aaa",2);//出错,它本应该触发bbb,但没有警告,它只要求你重来
+            flow.order("aaa",3);//没有反应
+            flow.order("bbb",4);//没有反应
+            flow.order("ccc",5);//[4,5,6]
+        })
+        //如果new $.Flow(4000)在构器器传入timeout,在第一个fire后,过了4秒时间限制还没有全部依次触发完aaa,bbb,ccc
+        //也要求你重来
+         */
+        order: function(type){//
+            if(this._events[type]){
+                var cur = this._queue.shift();
+                if(!this.timestamp ){
+                    this.timestamp  = new Date - 0
+                }
+                var limit = true;
+                if(this.timeout && (new Date - this.timestamp > this.timeout)){
+                    limit = false;
+                }
+                if(type == cur && limit){
+                    this.fire.apply(this,arguments)
+                }else{
+                    this._queue = this._order.concat();
+                    this._fired = {}
+                    delete this.timestamp
+                }
+            }
+        },
         /**一个子步骤在重复执行N遍后,执行最后的回调
             require("interact",function($){
                 var flow = new $.Flow;
@@ -180,7 +217,7 @@ define("interact",["$class"], function($){
             return this;
         }
     })
-
+    
     $.Flow.create = function (names, callback, errorback) {
         var that = new $.Flow;
         var args = names.match($.rword) || [];
@@ -197,10 +234,12 @@ define("interact",["$class"], function($){
         times = 0,
         uniq = {},
         events =  name.match($.rword) ,
-        length = events.length
+        length = events.length;
         if(!events.length){
             return this;
         }
+        this._queue = events.concat();
+        this._order = events.concat();
         function bind(key) {
             flow.bind(key, function () {
                 if (!uniq[key]) {
@@ -227,7 +266,7 @@ define("interact",["$class"], function($){
                 uniq = {};
                 times = 0;
             }
-            callback.apply(null, result);
+            callback.apply(flow, result);
         }
         flow.bind(last, lastFn);
     };
