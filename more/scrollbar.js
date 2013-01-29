a6f20a070a37647bae5f1b27ba0cd49aaab67be3
RubyLouvre <cheng19840218@gmail.com>
2013-01-29 12:25:18 +0800

    update scrollbar模块

--- more/scrollbar.js
+++ more/scrollbar.js
@@ -1,18 +1,38 @@
-define("scrollbar", ["css"],function( $){
-    $.fn.hasScrollBar = function() {
-        var node = this[0],ret = false;
-        if ((node.clientHeight < node.scrollHeight) || (node.clientWidth < node.scrollWidth)) {
-            ret = true;
+define("scrollbar", ["css"], function($) {
+    function hasScrollBar(target, key) {
+        //判定是否存在水平或垂直滚动条
+        var val = target.css('overflow-' + key)
+        if(val == 'scroll') return true;
+        if(val == 'hidden') return false;
+        if(val == 'auto') {
+            var el = target[0],
+                method = key == 'y' ? 'Height' : 'Width';
+            return el['client' + method] < el['scroll' + method];
         }
-        return ret;
+        return false
     }
-    $.scrollbarWidth = function (){
-        if( $.scrollbarWidth.ret ){
-            return $.scrollbarWidth.ret
+    $.fn.hasScrollBar = function() {
+        //判定当前匹配的第一个元素是否存在滚动条
+        return isDisplayScrollBar(this, 'x') || isDisplayScrollBar(this, 'x');
+    }
+    $.getDefaultScrollBarWidth = function() {
+        //取得当前窗口默认的滚动条的宽度
+        if($.getDefaultScrollBarWidth.ret) {
+            return $.getDefaultScrollBarWidth.ret
         }
-        var test =  $('<div style="width: 100px;height: 100px;overflow: scroll;position: absolute;top: -9999px;"/>').appendTo("body");
+        var test = $('<div style="width: 100px;height: 100px;overflow: scroll;position: absolute;top: -9999px;"/>').appendTo("body");
         var ret = test[0].offsetWidth - test[0].clientWidth;
         test.remove();
-        return $.scrollbarWidth.ret = ret;
+        return $.getDefaultScrollBarWidth.ret = ret;
     }
+    $.getScrollBarWidth = function(node) {
+        //获取当前元素的滚动条的消息
+        var target = $(node),
+            width = $.getDefaultScrollBarWidth();
+        return {
+            x: hasScrollBar(target, 'x') ? width : 0,
+            y: hasScrollBar(target, 'y') ? width : 0
+        }
+    }
+    return $;
 });
